67ca9bc8e0f094f3f2cc300e0f46d414dac6647c
efren <efrenfloresp@gmail.com>
2023-11-29 23:41:55 -0600

    Update 2.1
    Added the sight transformations and will solve later the problem in the menu

--- mainCode.py
+++ mainCode.py
@@ -1,7 +1,8 @@
 # mainCode.py
 
-import cv2
+import cv2 as cv
 import numpy as np
+import matplotlib.pyplot as plt
 import pygame
 
 # Function to transform an image
@@ -26,19 +27,49 @@ def transform_image(image_path, animal_sight_type):
     return original_image, transformed_image
 
 def transform_to_dog_sight(image):
-    # Transform image to dog sight
-    # For now, just return the original image
-    return image
+    # Dicromatic vision
+    img = cv.imread(image)[:, :, :: -1]
+
+    # Delete red channel
+    delete_red = img.copy()
+    delete_red[:, :, 2] = 0
+
+    # Merge blue and green channels
+    final_img = delete_red[:, :, [1, 0, 0]]
+
+    return final_img
 
 def transform_to_bee_sight(image):
-    # Replace this with actual transformation logic for bee sight
-    # For now, just return the original image
-    return image
+    # Ultra violet vision
+    img = cv.imread(image)
+
+    # Split channels
+    b, g, r = cv.split(img)
+
+    # Convert channels to NumPy arrays
+    b = np.array(b, dtype=np.float32)
+    g = np.array(g, dtype=np.float32)
+    r = np.array(r, dtype=np.float32)
+
+    # Apply modifications to simulate fluorescence
+    uv_image = img.copy()
+    uv_image[:, :, 0] = b * 0.5  # Adjusting the blue channel
+    uv_image[:, :, 1] = g * 0.5  # Adjusting the green channel
+    uv_image[:, :, 2] = r * 1.5  # Adjusting the red channel
+
+    final_img = cv.cvtColor(uv_image, cv.COLOR_BGR2RGB)
+    return final_img
 
 def transform_to_bat_sight(image):
-    # Replace this with actual transformation logic for bat sight
-    # For now, just return the original image
-    return image
+    # Eco location
+    img_gris = cv.imread(image, 0)
+    laplce1 = np.array([
+    [0, 1, 0],
+    [1, -4, 1],
+    [0, 1, 0]
+    ])
+    final_img = cv.filter2D(img_gris, -1, laplce1)
+    return final_img
 
 def transform_to_snake_sight(image):
     # Replace this with actual transformation logic for snake sight
