cfa72d7f33b55abb72695e9df9c8d74dc94b2a16
efren <efrenfloresp@gmail.com>
2023-12-02 07:39:31 -0600

    Update snake sight

--- mainCode.py
+++ mainCode.py
@@ -1,6 +1,5 @@
 # mainCode.py
 
-from typing import final
 import cv2 as cv
 import numpy as np
 import matplotlib.pyplot as plt
@@ -15,7 +14,7 @@ def transform_to_dog_sight(image):
     # Merge blue and green channels
     final_img = delete_red[:, :, [1, 0, 0]]
 
-    return final_img
+    return cv.cvtColor(final_img, cv.COLOR_BGR2RGB)
 
 def transform_to_bee_sight(image):
     # Ultra violet vision
@@ -33,8 +32,7 @@ def transform_to_bee_sight(image):
     uv_image[:, :, 1] = g * 0.5  # Adjusting the green channel
     uv_image[:, :, 2] = r * 1.5  # Adjusting the red channel
 
-    final_img = cv.cvtColor(uv_image, cv.COLOR_BGR2RGB)
-    return final_img
+    return cv.cvtColor(uv_image, cv.COLOR_BGR2RGB)
 
 def transform_to_bat_sight(image):
     # Eco location
@@ -47,7 +45,20 @@ def transform_to_bat_sight(image):
     final_img = cv.filter2D(img_gris, -1, laplce1)
     return final_img
 
-def transform_to_snake_sight(image):
-    # Replace this with actual transformation logic for snake sight
-    # For now, just return the original image
-    return image
+def transform_to_snake_sight(img_link):
+    img = cv.imread(img_link)
+
+    # Split channels
+    b, g, r = cv.split(img)
+
+    # Convert channels to NumPy arrays
+    b = np.array(b, dtype=np.float32)
+    g = np.array(g, dtype=np.float32)
+    r = np.array(r, dtype=np.float32)
+    
+    infrared_img = img.copy()
+    infrared_img[:, :, 0] = b * 0.5
+    infrared_img[:, :, 1] = g * 1.5
+    infrared_img[:, :, 2] = r * 3
+
+    return cv.cvtColor(infrared_img, cv.COLOR_BGR2RGB)
